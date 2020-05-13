52e119550810e8e13c0f420534d7918d250c2ab2
unknown <macjay314@gmail.com>
2020-05-13 13:32:58 -0700

    Add final test and functionality

--- RPC.Tests/ModelTests/GameTests.cs
+++ RPC.Tests/ModelTests/GameTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using RPC.Models;
@@ -5,8 +6,13 @@ using RPC.Models;
 namespace RPC.Tests
 {
   [TestClass]
-  public class GameTests
+  public class GameTests : IDisposable
   {
+    public void Dispose()
+    {
+      Game.ClearAll();
+    }
+    
     [TestMethod]
     public void Game_CreatesGameInstanceWithTwoPlayers_List()
     {
@@ -18,6 +24,31 @@ namespace RPC.Tests
       List<Player> theList = Game.GetAll();
 
       CollectionAssert.AreEqual(theList, compareList);
+    }
+
+    [TestMethod]
+
+    public void PlayGame_ReturnsATie_String()
+    {
+      Player playerOne = new Player("rock", true);
+      Player playerTwo = new Player("rock", false);
+      Game theGame = new Game(playerOne, playerTwo);
+      
+      string tempWinner = Game.PlayGame();
+
+      Assert.AreEqual(tempWinner, "tie");
+    }
+
+    [TestMethod]
+    public void PlayGame_ReturnsPaperBeatsRock_String()
+    {
+      Player playerOne = new Player("scissors", true);
+      Player playerTwo = new Player("paper", false);
+      Game theGame = new Game(playerOne, playerTwo);
+
+      string tempWinner = Game.PlayGame();
+
+      Assert.AreEqual(tempWinner, "scissors beats paper");
     }
   }
 }
