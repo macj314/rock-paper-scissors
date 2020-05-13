8e8229a979b04396763e971722fa64523fa5303f
unknown <macjay314@gmail.com>
2020-05-13 11:26:36 -0700

    Set a list of player objects as a property of game. Create a constructor that fills that list. Create a method to retrieve that list

--- RPC/Models/Game.cs
+++ RPC/Models/Game.cs
@@ -1,12 +1,18 @@
+using System.Collections.Generic;
 namespace RPC.Models
 {
   public class Game
   {
-    public List<Player> playerList = new List<Player>{};
+    private static List<Player> _playerList = new List<Player>{};
     public Game(Player player1, Player player2)
     {
-      playerList.Add(player1);
-      playerList.Add(player2);
+      _playerList.Add(player1);
+      _playerList.Add(player2);
+    }
+
+    public static List<Player> GetAll()
+    {
+      return _playerList;
     }
   }
 }
